2ed7bc6d5c0b5c8338fa2f034f151b426757dfa3
fedemengo <mengozzi.federico@gmail.com>
2018-12-24 04:45:18 -0800

    Clean up

--- .zsh/blox.zsh
+++ .zsh/blox.zsh
@@ -3,8 +3,9 @@ ZSH_THEME="blox-zsh-theme/blox"
 
 # Define some colors
 LIGHT_GREEN=76
-LIGHT_BLUE=38
+LIGHT_BLUE=39
 GREEN=2
+YELLOW=11
 
 BLOX_CONF__PROMPT_PREFIX=""
 
@@ -23,14 +24,14 @@ BLOX_BLOCK__HOST_USER_COLOR=${LIGHT_GREEN}
 BLOX_BLOCK__GIT_DIRTY_SYMBOL="?"
 BLOX_BLOCK__GIT_STASHED_SYMBOL="\U235F"
 BLOX_BLOCK__GIT_BRANCH_COLOR="green"
-#BLOX_BLOCK__GIT_COMMIT_COLOR="gray"
+BLOX_BLOCK__GIT_COMMIT_COLOR="gray"
 BLOX_BLOCK__GIT_STASHED_COLOR="gray"
 
 BLOX_BLOCK__DIR_COLOR=${LIGHT_BLUE}
 BLOX_BLOCK__DIR_LEN=3
 
 
-function blox_block__user_host() {
+function blox_block__my_user_host() {
 	local user_color=$BLOX_BLOCK__HOST_USER_COLOR
 
 	[[ $USER == "root" ]] && user_color=$BLOX_BLOCK__HOST_USER_ROOT_COLOR
@@ -92,19 +93,15 @@ function blox_block__my_dir() {
 # ---------------------------------------------
 # Git block options
 
-# Colors
-BLOX_BLOCK__GIT_BRANCH_COLOR="${BLOX_BLOCK__GIT_BRANCH_COLOR:-242}"
-
 # Commit hash
 BLOX_BLOCK__GIT_COMMIT_SHOW="${BLOX_BLOCK__GIT_COMMIT_SHOW:-true}"
-BLOX_BLOCK__GIT_COMMIT_COLOR="${BLOX_BLOCK__GIT_COMMIT_COLOR:-magenta}"
 
 # Clean
 BLOX_BLOCK__GIT_CLEAN_COLOR="${BLOX_BLOCK__GIT_CLEAN_COLOR:-green}"
 BLOX_BLOCK__GIT_CLEAN_SYMBOL="${BLOX_BLOCK__GIT_CLEAN_SYMBOL:-✔}"
 
 # Dirty
-BLOX_BLOCK__GIT_DIRTY_COLOR="11"
+BLOX_BLOCK__GIT_DIRTY_COLOR=${YELLOW}
 BLOX_BLOCK__GIT_DIRTY_SYMBOL="!"
 BLOX_BLOCK__GIT_DIRTY_COLOR="${BLOX_BLOCK__GIT_DIRTY_COLOR:-yellow}"
 BLOX_BLOCK__GIT_DIRTY_SYMBOL="${BLOX_BLOCK__GIT_DIRTY_SYMBOL:-!}"
@@ -113,7 +110,7 @@ BLOX_BLOCK__GIT_DIRTY_SYMBOL="${BLOX_BLOCK__GIT_DIRTY_SYMBOL:-!}"
 BLOX_BLOCK__GIT_UNTRACKED_COLOR="${BLOX_BLOCK__GIT_UNTRACKED_COLOR:-red}"
 BLOX_BLOCK__GIT_UNTRACKED_SYMBOL="${BLOX_BLOCK__GIT_UNTRACKED_SYMBOL:-?}"
 
-BLOX_BLOCK__GIT_UNPUSHED_COLOR="39"
+BLOX_BLOCK__GIT_UNPUSHED_COLOR=${LIGHT_BLUE}
 
 # ---------------------------------------------
 # Themes
@@ -129,21 +126,22 @@ BLOX_BLOCK__GIT_THEME_UNPUSHED="%F{${BLOX_BLOCK__GIT_UNPUSHED_COLOR}]%}$BLOX_BLO
 # Helper functions
 
 function set_commit_color() {
-    local repo_status=$1
+    local st=$1
     local color=${BLOX_BLOCK__GIT_CLEAN_COLOR}
-    (((repo_status & 1) == 1)) && color=${BLOX_BLOCK__GIT_UNTRACKED_COLOR}
-    (((repo_status & 2) == 2)) && color=${BLOX_BLOCK__GIT_DIRTY_COLOR}
+    (((st & 1) == 1)) && color=${BLOX_BLOCK__GIT_UNTRACKED_COLOR}
+    (((st & 2) == 2)) && color=${BLOX_BLOCK__GIT_DIRTY_COLOR}
     echo "${color}"
 }
 
 # Echo the appropriate symbol for branch's status
 function my_status() {
-  if [[ -z "$1" ]]; then
-    echo " $BLOX_BLOCK__GIT_THEME_CLEAN"
-  else
-    (((repo_status & 2) == 2)) && echo -n " $BLOX_BLOCK__GIT_THEME_DIRTY"
-    (((repo_status & 1) == 1)) && echo -n " $BLOX_BLOCK__GIT_THEME_UNTRACKED"
-  fi
+    local st=$1
+    if [[ -z "$1" ]]; then
+        echo " $BLOX_BLOCK__GIT_THEME_CLEAN"
+    else
+        (((st & 2) == 2)) && echo -n " $BLOX_BLOCK__GIT_THEME_DIRTY"
+        (((st & 1) == 1)) && echo -n " $BLOX_BLOCK__GIT_THEME_UNTRACKED"
+    fi
 }
 
 # Echo the appropriate symbol if there are stashed files
@@ -157,27 +155,25 @@ function my_stashed_status() {
 # 0 repo is clean
 # 1 repo has untracked files
 # 2 repo is dirty
-repo_status() {
+function my_repo_status() {
     local result=0
-    local repo_status=$(git status -s)
-    if [[ -z "${repo_status}" ]]; then
-        #
-    else
+    local st=$(git status -s)
+    if [[ -n "${st}" ]]; then
         while read -r line; do
             #if [[ "${line:0:1}" == "?" || "${line:1:1}" == "?" ]]; then
             if [[ "${line:1:1}" == "?" ]]; then     # locally untracked
                 result=$((result + 1))
                 break
             fi
-        done <<< "${repo_status}"
+        done <<< "${st}"
 
         while read -r line; do 
-            #if [[ "${line:0:1}" == "M" || "${line:1:1}" == "M" ]]; then
-            if [[ "${line:1:1}" == "M" ]]; then     # locally modified
+            if [[ "${line:0:1}" == "M" || "${line:1:1}" == "M" ]]; then
+            #if [[ "${line:1:1}" == "M" ]]; then     # locally modified
                 result=$((result + 2))
                 break
             fi
-        done <<< "${repo_status}"
+        done <<< "${st}"
     fi
     echo ${result}
 }
@@ -191,11 +187,11 @@ function blox_block__my_git() {
 
     local commit_hash
     
-    local repo_status=$(repo_status)
-    local commit_color=$(set_commit_color ${repo_status})
+    local st=$(my_repo_status)
+    local commit_color=$(set_commit_color ${st})
 
     local branch_name="$(blox_block__git_helper__branch)"
-    local branch_status="$(my_status  ${repo_status})"
+    local branch_status="$(my_status  ${st})"
     local stashed_status="$(my_stashed_status)"
     local remote_status="$(blox_block__git_helper__remote_status)"
 
@@ -215,6 +211,7 @@ function blox_block__my_git() {
   fi
 }
 
-BLOX_SEG__UPPER_LEFT=(user_host dir my_git)
+BLOX_SEG__UPPER_LEFT=(my_user_host my_dir my_git)
 BLOX_SEG__UPPER_RIGHT=(exec_time bgjobs time)
 BLOX_SEG__LOWER_LEFT=(symbol)
+
