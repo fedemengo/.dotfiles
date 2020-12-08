7bf529392cd49bec93dcd53707b7b0e28609e2a3
fedemengo <19249682+fedemengo@users.noreply.github.com>
2020-12-08 23:14:38 +0100

    Fix polybar hangs

--- .config/polybar/ip-info.sh
+++ .config/polybar/ip-info.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-notify-send -u normal "`curl -u ${IP_TOKEN}: ipinfo.io -H 'Cache-Control: no-cache' 2>/dev/null | sed -n '2p;4,5p;7,8p' | sed 's/",/"/g;s/^\ *//g'`" -t 10000
+notify-send -u normal "`curl -u ${IP_TOKEN}: ipinfo.io -H 'Cache-Control: no-cache' 2>/dev/null | sed -n '2p;4,5p;7,8p' | sed 's/",/"/g;s/^\ *//g;s/\"//g' | awk 'BEGIN {print "IP Info"}; {split($0,v,":"); printf "%-8s\t\t%35s\n", v[1], v[2]}'`" -t 10000 &
 
